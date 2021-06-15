48e4e717c53e1b5633d8e28aeaeafe429fc2b043
Bharti kumari <bhartikumari20@navgurukul.org>
2021-06-15 06:27:46 +0530

    array logical questions

---
+++ arrayLogicalQuestions/findDublicates.js
@@ -0,0 +1,20 @@
+let list = ['aba', 'bca', 'dadd', 'bb']
+let i = 0
+while(i<list.length) {
+    if(list[i].length > 2 && list[i][0] == list[i][list[i].length -1]){
+            console.log('1')
+    }i++
+}
+
+
+//In two loop
+// let list = ['aba', 'bca', 'dadd', 'bb']
+// let i = 0
+// while(i<list.length) {
+//     let j = 0
+//     while(j<=0) {
+//         if(list[i].length > 2 && list[i][j] == list[i][list[i].length -1]){
+//             console.log('1')
+//         }j++
+//     }i++
+// }
---
+++ arrayLogicalQuestions/typeOfElement.js
@@ -0,0 +1,14 @@
+let array = ['bharti', '4', 'neha', '8', 'jyoti', '0', '-1']
+let array2 = []
+let i = 0
+if(Number(array[i]) == "NaN"){
+    console.log('string')
+}
+// while(i<array.length) {
+//     if(Number(array[i]) == 'NaN') {
+//         array2.push('string')
+//     }else{
+//         array2.push('number')
+//     }
+//     i++
+// }console.log(array2)
