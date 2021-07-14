9ba413957b992e011825ee952a9ab407bf96a1de
Bharti kumari <bhartikumari20@navgurukul.org>
2021-07-14 07:59:06 +0530

    consolealphabets

---
+++ loopQuestions/asciiValue.js
@@ -0,0 +1,6 @@
+let i = 65
+let index = 90
+while(i<=index) {
+    console.log(String.fromCharCode(i) )
+    i++
+};
--- loopQuestions/twistedPrime.js
+++ loopQuestions/twistedPrime.js
@@ -1,34 +1,38 @@
 const input = require('readline-sync')
 let num = input.question('Enter the number---')
 
-let i = 1
-let count = 0
-while(i<=num) {
-    if(num%i == 0) {
-        count = count+1
-    }i++
+let checkPrime = function (number) {
+    let i = 1
+    let count = 0
+    while(i<=number) {
+        if(number%i == 0) {
+            count = count+1
+        }i++
+    };
+    return count
 };
-if(count == 2) {
-    var str = ""
-    while(num>0) {
-        var s = num%10
-        str = str+s
-        num = Math.floor(num/10)
-    }var int = Number(str)
-    let z = 1
-    let count1 = 0
-    while(z<=int) {
-        if(int%z == 0) {
-            count1 = count1+1
-        }z++
-    }
-    if(count1 == 2) {
-        console.log('It is twisted prime number')
+let reverseNum = function () {
+    let value = checkPrime(num)
+    if(value == 2) {
+        var str = ""
+        while(num>0) {
+            var s = num%10
+            str = str+s
+            num = Math.floor(num/10)
+        };
+        var int = Number(str)
+        let secondValue = checkPrime(int)
+        if(secondValue == 2) {
+            console.log('Twisted prime')
+        }else{
+            console.log('Not a twisted prime')
+        };
+
     }else{
-        console.log('Not twisted prime')
-    }  
-}
-else{
-    console.log('Not prime')
-}
+        console.log('Not Prime number')
+    };
+};
+reverseNum();
+
+
 
